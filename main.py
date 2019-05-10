a4242bed8fb49de96f3e5973e70b79d223a38d8f
lolMinsoo <byang18@illinois.edu>
2019-05-09 20:15:38 -0700

    Update main.py
    Fixed D1

--- main.py
+++ main.py
@@ -7,22 +7,40 @@ API_KEY = ''
 current_date = datetime.today().strftime('%Y-%m-%d')
 
 
+
+
+def main():
+	print('------------------------------')
+	print('1. Get ranked data')
+	
+	# TODO: Need to implement checks
+	if input() == '1':
+		region = input('Region: ')
+		print(region)
+		get_ranked_data(API_KEY, region)
+
 def get_ranked_data(apikey, region):
-	# Open File
+	
+	TOTAL_PLAYERS = 0
 	filename = current_date + "_{}_DATA.csv".format(region)
 	file = open(filename, "w+", encoding='utf-8')
 	file.close()
 	
-	diamond_data(apikey, region)
-	master_data(apikey, region)
-	grandmaster_data(apikey, region)
-	challenger_data(apikey, region)
+	TOTAL_PLAYERS = diamond_data(apikey, region, TOTAL_PLAYERS)
+	TOTAL_PLAYERS = master_data(apikey, region, TOTAL_PLAYERS)
+	TOTAL_PLAYERS = grandmaster_data(apikey, region, TOTAL_PLAYERS)
+	TOTAL_PLAYERS = challenger_data(apikey, region, TOTAL_PLAYERS)
 	
-def master_data(apikey, region):
+	print('{} players analyzed'.format(TOTAL_PLAYERS))
+	
+def master_data(apikey, region, TOTAL_PLAYERS):
 	URL = 'https://{region}.api.riotgames.com/lol/league/v4/masterleagues/by-queue/RANKED_SOLO_5x5?api_key={key}'
 	
 	get_data = requests.get(URL.format(region=region, key=apikey))
 	json_data = get_data.json()
+	
+	print('Gathering {} Master players data.'.format(str(len(json_data['entries']))))
+
 	
 	filename = current_date + "_{}_DATA.csv".format(region)
 	file = open(filename, "a+", encoding='utf-8')
@@ -31,12 +49,17 @@ def master_data(apikey, region):
 		file.write(json_data['entries'][i]['summonerName'] + ', ' + json_data['entries'][i]['summonerId']
 		+ ', MASTER I, ' + str(json_data['entries'][i]['leaguePoints']) + '\n')
 	file.close()
+	
+	return TOTAL_PLAYERS
 		
-def challenger_data(apikey, region):
+def challenger_data(apikey, region, TOTAL_PLAYERS):
 	URL = 'https://{region}.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key={key}'
 	
 	get_data = requests.get(URL.format(region=region, key=apikey))
 	json_data = get_data.json()
+	
+	print('Gathering {} Challenger players data.'.format(str(len(json_data['entries']))))
+	TOTAL_PLAYERS += len(json_data['entries'])
 	
 	filename = current_date + "_{}_DATA.csv".format(region)
 	file = open(filename, "a+", encoding='utf-8')
@@ -45,13 +68,17 @@ def challenger_data(apikey, region):
 		file.write(json_data['entries'][i]['summonerName'] + ', ' + json_data['entries'][i]['summonerId']
 		+ ', CHALLENGER I, ' + str(json_data['entries'][i]['leaguePoints']) + '\n')
 	file.close()
+	return TOTAL_PLAYERS
 		
-def grandmaster_data(apikey, region):
+def grandmaster_data(apikey, region, TOTAL_PLAYERS):
 	URL = 'https://{region}.api.riotgames.com/lol/league/v4/grandmasterleagues/by-queue/RANKED_SOLO_5x5?api_key={key}'
 	
 	get_data = requests.get(URL.format(region=region, key=apikey))
 	json_data = get_data.json()
 
+	print('Gathering {} Grandmaster players data.'.format(str(len(json_data['entries']))))
+	TOTAL_PLAYERS += len(json_data['entries'])
+	
 	filename = current_date + "_{}_DATA.csv".format(region)
 	file = open(filename, "a+", encoding='utf-8')
 	
@@ -59,19 +86,31 @@ def grandmaster_data(apikey, region):
 		file.write(json_data['entries'][i]['summonerName'] + ', ' + json_data['entries'][i]['summonerId']
 		+ ', GRANDMASTER I, ' + str(json_data['entries'][i]['leaguePoints']) + '\n')
 	file.close()
+	return TOTAL_PLAYERS
 
-def diamond_data(apikey, region):
-	URL = 'https://{region}.api.riotgames.com/lol/league/v4/entries/RANKED_SOLO_5x5/DIAMOND/I?api_key={key}'
+def diamond_data(apikey, region, TOTAL_PLAYERS):
+	URL = 'https://{region}.api.riotgames.com/lol/league/v4/entries/RANKED_SOLO_5x5/DIAMOND/I?page={page}&api_key={key}'
 	
-	get_data = requests.get(URL.format(region=region, key=apikey))
-	json_data = get_data.json()
-
 	filename = current_date + "_{}_DATA.csv".format(region)
 	file = open(filename, "a+", encoding='utf-8')
 	
-	for i in range(len(json_data)):
-		file.write(json_data[i]['summonerName'] + ', ' + json_data[i]['summonerId']
-		+ ', DIAMOND I, ' + str(json_data[i]['leaguePoints']) + '\n')
-	file.close()
+	for page in range(1, 50):
+	
+		json_data = requests.get(URL.format(region=region, page=page, key=apikey)).json()
+		TOTAL_PLAYERS += len(json_data)
 		
-get_ranked_data(API_KEY, "NA1")
+		if page == 1:
+			print('Gathering first {} Diamond I players data.'.format(str(len(json_data))))
+		elif len(json_data) > 1:
+			print('Gathering next {} Diamond I players data.'.format(str(len(json_data))))
+		
+		if len(json_data) < 1:
+			break
+		for i in range(len(json_data)):
+			file.write(json_data[i]['summonerName'] + ', ' + json_data[i]['summonerId'] 
+			+ ', DIAMOND I, ' + str(json_data[i]['leaguePoints']) + '\n')
+	file.close()
+	return TOTAL_PLAYERS	
+	
+
+main()
